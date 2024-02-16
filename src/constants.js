41b81d243ca0ecf39550245be294ce59acf9679c
kevinsoras <kevinalexandersoras@gmail.com>
2024-02-16 11:18:58 -0500

    fix: fix route

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 
-export const URL_BASE = "https://b5ad-38-137-200-15.ngrok-free.app ";
+export const URL_BASE = "https://b5ad-38-137-200-15.ngrok-free.app";
 export const tokenKey = "scvd-auth-token";
